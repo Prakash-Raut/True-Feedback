e9c97bb74ccc44c5b2d824d43e612dcfe72f3abc
Prakash Raut <prakash_raut@outlook.com>
2024-05-14 17:27:18 +0530

    add: check-username and verify code

---
+++ src/app/api/check-unique-username/route.ts
@@ -0,0 +1,80 @@
+import { dbConnect } from "@/lib/dbConnect";
+import UserModel from "@/models/User.model";
+import { usernameValidation } from "@/schemas/signup.schema";
+import { z } from "zod";
+
+const UsernameQuerySchema = z.object({
+	username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+	if (request.method !== "GET") {
+		return Response.json(
+			{
+				success: false,
+				message: "Method not allowed",
+			},
+			{ status: 405 }
+		);
+	}
+
+	await dbConnect();
+
+	try {
+		const { searchParams } = new URL(request.url);
+		const queryParam = {
+			username: searchParams.get("username"),
+		};
+		// validate with zod
+		const result = UsernameQuerySchema.safeParse(queryParam);
+		console.log("Unique username check :: Username Query Schema ", result);
+		if (!result.success) {
+			const usernameErrors =
+				result.error.format().username?._errors || [];
+			return Response.json(
+				{
+					success: false,
+					message:
+						usernameErrors?.length > 0
+							? usernameErrors.join(", ")
+							: "Invliad query parameters",
+				},
+				{ status: 400 }
+			);
+		}
+
+		const { username } = result.data;
+
+		const existingVerifiedUser = await UserModel.findOne({
+			username,
+			isVerified: true,
+		});
+
+		if (existingVerifiedUser) {
+			return Response.json(
+				{
+					success: false,
+					message: "Username already taken",
+				},
+				{ status: 400 }
+			);
+		}
+
+		return Response.json(
+			{
+				success: true,
+				message: "Username available",
+			},
+			{ status: 400 }
+		);
+	} catch (error) {
+		console.error("Error checking unique username", error);
+		return Response.json(
+			{
+				success: false,
+				message: "Error checking username",
+			},
+			{ status: 500 }
+		);
+	}
+}
